8c7b24c2454849fc53bf0e0ec72e335a9a9a4170
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-16 18:24:21 -0600

    comment model validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Comment do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = Comment.new()
+        expect(comment).to be_invalid
+      end
+    end
+  end
 end
