050231d6982704d24b89b62efd98710a2739db4e
Jim Szalewski <jim.szalewski@gmail.com>
2017-07-16 23:10:14 -0600

    contact model validations

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Contact do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a name" do
+        contact = Contact.new(position: "HR", email: "j@gmail.com")
+        expect(contact).to be_invalid
+      end
+
+      it "is invalid without a position" do
+        contact = Contact.new(name: "JJ", email: "j@gmail.com")
+        expect(contact).to be_invalid
+      end
+
+      it "is invalid without an email" do
+        contact = Contact.new(name: "JJ", position: "HR")
+        expect(contact).to be_invalid
+      end
+    end
+  end
 end
